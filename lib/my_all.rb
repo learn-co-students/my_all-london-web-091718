e8e45bd555991b9315697b8a83b1d294fc4fb9c0
ssong-eu <sangyoon.song@gmail.com>
2018-09-20 10:11:52 +0100

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,15 @@
 require 'pry'
 
 def my_all?(collection)
-
+  i = 0
+  number_of_truths = 0
+  # if collection.empty? == false
+    while i < collection.length
+      if yield collection[i]
+        number_of_truths += 1
+      end
+      i += 1
+    end
+  # end
+  return number_of_truths == collection.length
 end
